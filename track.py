c670ab8f47fef8d31665993a15638953bcd96a97
dl923 <dl923@cornell.edu>
2018-03-25 18:20:46 -0400

    Updated tracking list - first 10 nationals games

--- track.py
+++ track.py
@@ -1,4 +1,4 @@
 #place this file in the same folder as your matterhorn.py script
 #you can add multiple tags to track
 #the tags are the event IDs, which you can pull from the stubhub url
-tags = ['103240207']
+tags = ['103240207', '103240209', '103240211', '103240213', '103240215', '103240217', '103240225', '103240228', '103240230', '103240232']
